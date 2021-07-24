7579443a7fa124d242418e8da727ba91b56ff218
harshada537 <87888611+harshada537@users.noreply.github.com>
2021-07-24 11:37:13 +0530

    Test

---
+++ FileData/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string outputVersion="";
+            int outputSize = 0;
+            string firstArgs=args[0].ToString();
+            FileDetails filesystem = new FileDetails();
+
+            if (firstArgs == "-v" || firstArgs == "--v" || firstArgs == "/v" || firstArgs == "--version")
+            {
+                outputVersion = filesystem.Version(args[1].ToString());
+                Console.WriteLine(outputVersion);
+            }
+            else if (firstArgs == "-s" || firstArgs == "--s" || firstArgs == "/s" || firstArgs == "--size")
+            {
+                outputSize = filesystem.Size(args[1].ToString());
+                Console.WriteLine(outputSize);
+            }
+            else
+            {
+                Console.WriteLine("Please provide correct argument ");
+            }
+          
+           
+        }
+    }
+}
